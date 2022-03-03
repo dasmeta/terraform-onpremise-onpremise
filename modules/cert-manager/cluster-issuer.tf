1d60cd5879300963a337f0bcb7012f70ad2e6f24
aghamyan44 <juliaaghamyan44@gmail.com>
2022-03-03 15:09:28 +0400

    Change k8s MANIFEST

--- modules/cert-manager/cluster-issuer.tf
+++ modules/cert-manager/cluster-issuer.tf
@@ -1,67 +1,30 @@
 locals {
-  solvers = concat(
-    var.r53_configs.access_key_id == "" ? [] : [{
-      "dns01" = {
-        "route53" = {
-          "region"       = var.r53_configs.region
-          "accessKeyID"  = var.r53_configs.access_key_id
-          "hostedZoneID" = var.r53_configs.hosted_zone_id
-          "secretAccessKeySecretRef" = {
-            "name" = "${var.clusterissuer_name}-aws-r53-secret"
-            "key"  = "secret-access-key"
-          }
-        }
-      }
-    }],
-    var.gcloud_configs.project_id == "" ? [] : [{
-      "apiVersion" = "cert-manager.io/v1"
-      "kind"       = "ClusterIssuer"
-      "metadata" = {
-        "name" = var.clusterissuer_name
-      }
-      "spec" = {
-        "acme" = {
-          "email" = var.email_address
-          "privateKeySecretRef" = {
-            "name" = "letsencrypt-prod-gcp"
-          }
-          "server" = "https://acme-v02.api.letsencrypt.org/directory"
-          "solvers" = [
-            {
-              "dns01" = {
-                "cloudDNS" = {
-                  "project" = var.gcloud_configs.project_id
-                  "serviceAccountSecretRef" = {
-                    "name" = "${var.clusterissuer_name}-gcloud-dns-secret"
-                    "key"  = "key.json"
-                  }
-                }
-              }
-            },
-          ]
-        }
-      }
-    }],
-    [{
-      "http01" = {
-        "ingress" = {
-          "class" = "nginx"
-        }
-      }
-    }]
-  )
-}
-
-data "kubectl_path_documents" "cluster_issuer" {
-  pattern = "${path.module}/cluster_issuer.yaml"
-  vars = {
-    clusterissuer_name = var.clusterissuer_name
-    email_address      = var.email_address
-    solvers            = local.solvers
+  route53_vars = {
+    clusterissuer_name            = var.clusterissuer_name
+    email_address                 = var.email_address
+    region                        = var.r53_configs.region
+    accesskeyid                   = var.r53_configs.access_key_id
+    hostedzoneid                  = var.r53_configs.hosted_zone_id
+    secretaccesskeysecretref_name = "${var.clusterissuer_name}-aws-r53-secret"
+    secretaccesskeysecretref_key  = "key.json"
+  }
+  gcloud_vars = {
+    clusterissuer_name            = var.clusterissuer_name
+    email_address                 = var.email_address
+    project                       = var.gcloud_configs.project_id
+    secretaccesskeysecretref_name = "${var.clusterissuer_name}-gcloud-dns-secret"
+    secretaccesskeysecretref_key  = "key.json"
   }
 }
 
+data "kubectl_path_documents" "cluster_issuer" {
+  count = var.r53_configs.access_key_id == "" ? 1 : 0
+
+  pattern = var.r53_configs.access_key_id == "" ? "${path.module}/cluster_issuer_gcloud.yaml" : "${path.module}/cluster_issuer_route53.yaml"
+  vars    = var.r53_configs.access_key_id == "" ? local.gcloud_vars : local.route53_vars
+}
+
 resource "kubectl_manifest" "cluster_issuer" {
-  count     = length(data.kubectl_path_documents.cluster_issuer.documents)
-  yaml_body = element(data.kubectl_path_documents.cluster_issuer.documents, count.index)
+  count     = length(data.kubectl_path_documents.cluster_issuer[0].documents)
+  yaml_body = element(data.kubectl_path_documents.cluster_issuer[0].documents, count.index)
 }
