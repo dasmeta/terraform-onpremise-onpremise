483a0375f4f819016b4f49783d3e4a4cb60c434b
aghamyan44 <juliaaghamyan44@gmail.com>
2022-03-09 14:54:03 +0400

    Loki

---
+++ modules/grafana-loki/variables.tf
@@ -0,0 +1,57 @@
+variable "app_version" {
+  type        = string
+  default     = "10.1.16"
+  description = "Minio App Chart Version"
+}
+
+variable "name" {
+  type        = string
+  default     = "minio"
+  description = "Application name"
+}
+
+variable "create_namespace" {
+  type        = bool
+  default     = true
+  description = "Create Namespace"
+}
+
+variable "force_update" {
+  type        = bool
+  default     = true
+  description = "Force Update"
+}
+
+variable "wait" {
+  type        = bool
+  default     = false
+  description = "Wait"
+}
+
+variable "recreate_pods" {
+  type        = bool
+  default     = false
+  description = "Recreate pods"
+}
+
+variable "deploy" {
+  type        = number
+  default     = 1
+  description = "Deploy"
+}
+variable "resources_limits" {
+  type        = any
+  default     = {}
+  description = "The resources container"
+}
+
+variable "resources_requests" {
+  type        = any
+  default     = {}
+  description = "The resources container"
+}
+
+variable "grafana_enabled" {
+  type    = bool
+  default = true
+}
