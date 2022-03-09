8d6a4cb24ce194773c74157ad17a1f3ab2015dbd
aghamyan44 <juliaaghamyan44@gmail.com>
2022-03-09 14:59:37 +0400

    File

---
+++ modules/keda/variables.tf
@@ -0,0 +1,57 @@
+variable "app_version" {
+  type        = string
+  default     = "2.5.1"
+  description = "Minio App Chart Version"
+}
+
+variable "name" {
+  type        = string
+  default     = "keda"
+  description = "Application name"
+}
+
+variable "create_namespace" {
+  type        = bool
+  default     = true
+  description = "Create Namespace"
+}
+
+variable "force_update" {
+  type        = bool
+  default     = true
+  description = "Force Update"
+}
+
+variable "wait" {
+  type        = bool
+  default     = false
+  description = "Wait"
+}
+
+variable "recreate_pods" {
+  type        = bool
+  default     = false
+  description = "Recreate pods"
+}
+
+variable "deploy" {
+  type        = number
+  default     = 1
+  description = "Deploy"
+}
+variable "resources_limits" {
+  type        = any
+  default     = {}
+  description = "The resources container"
+}
+
+variable "resources_requests" {
+  type        = any
+  default     = {}
+  description = "The resources container"
+}
+
+variable "grafana_enabled" {
+  type    = bool
+  default = true
+}
