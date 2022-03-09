1c937f853cdbc6e994983b9d42b19ae9f40cf03f
aghamyan44 <juliaaghamyan44@gmail.com>
2022-03-09 14:47:58 +0400

    Change

--- modules/fission/main.tf
+++ modules/fission/main.tf
@@ -1,27 +1,31 @@
-module fission {
-  source  = "terraform-module/release/helm"
-  version = "2.7.0"
+module "fission" {
+  source     = "terraform-module/release/helm"
+  version    = "2.7.0"
   namespace  = "fission"
-  repository =  "https://fission.github.io/fission-charts"
+  repository = "https://fission.github.io/fission-charts"
 
   app = {
-    name          = "fission"
-    version       = "1.15.1"
-    chart         = "fission-all"
-    create_namespace = true
-    force_update  = true
-    wait          = false
-    recreate_pods = false
-    deploy        = 1
+    name             = var.name
+    version          = var.app_version
+    chart            = "fission-all"
+    create_namespace = var.create_namespace
+    force_update     = var.force_update
+    wait             = var.wait
+    recreate_pods    = var.recreate_pods
+    deploy           = var.deploy
   }
-  set =  [
-  {
-    name = "influxdb.enabled"
-    value = "true"
-  },
-  { 
-    name = "persistence.enabled"
-    value = "false"
-  }
+  values = [
+    templatefile("${path.module}/values.yaml", { resources_requests_cpu = "${var.resources_requests.cpu}", resources_requests_memory = "${var.resources_requests.memory}", resources_limits_cpu = "${var.resources_limits.cpu}", resources_limits_memory = "${var.resources_limits.memory}" })
+  ]
+
+  set = [
+    {
+      name  = "influxdb.enabled"
+      value = var.influxdb_enabled
+    },
+    {
+      name  = "persistence.enabled"
+      value = var.persistence_enabled
+    }
   ]
 }
